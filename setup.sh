0e2af9c8fea2aa9f2ded9d6538808ff1cd2d8f90
Baloo54 <gabriel.comte2006@gmail.com>
2025-06-09 02:37:49 +0200

    pité marche

--- setup.sh
+++ setup.sh
@@ -13,6 +13,10 @@ docker secret rm db_password 2>/dev/null || true
 
 docker swarm init 2>/dev/null || true
 
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+-keyout nginx/certs/server.key -out nginx/certs/server.crt \
+-subj "/CN=localhost"
+
 echo "$DB_USER" | docker secret create db_user -
 echo "$DB_PASS" | docker secret create db_password -
 
